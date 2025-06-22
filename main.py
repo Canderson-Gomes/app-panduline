c003890df46127a85badc6f68c0e106827dfafba
Canderson-Gomes <candidogomescg@gmail.com>
2025-06-22 01:46:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from firebase_admin import credentials, initialize_app, storage
+import uuid
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ['http://localhost:3000']
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Inicializa Firebase
+cred = credentials.Certificate("firebase_config.json")
+initialize_app(cred, {
+    'storageBucket': 'https://api-imgs-panduline.firebaseapp.com/'
+})
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "API FastAPI no Render funcionando para o projeto?!"}
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    # Lê o conteúdo
+    contents = await file.read()
+    file_name = f"{uuid.uuid4()}_{file.filename}"
+
+    # Salva no Firebase
+    bucket = storage.bucket()
+    blob = bucket.blob(file_name)
+    blob.upload_from_string(contents, content_type=file.content_type)
+    blob.make_public()  # Torna o arquivo acessível publicamente
+
+    return {"file_url": blob.public_url}
