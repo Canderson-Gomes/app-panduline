a8d09c173e4e9837cfbb9fa1279deb62d11bda01
Canderson-Gomes <candidogomescg@gmail.com>
2025-07-04 19:54:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -59,25 +59,8 @@ async def upload_file(file: UploadFile = File(...)):
 
 
 '''
-
-  File "C:\Users\hp\Documents\devs\back-endPanduLine\venv\main.py", line 55, in upload_file
-    s3.upload_fileobj(
-    ~~~~~~~~~~~~~~~~~^
-        Fileobj=bytes(contents),
-        ^^^^^^^^^^^^^^^^^^^^^^^^
-        Bucket=BUCKET_NAME,
-        ^^^^^^^^^^^^^^^^^^^
-        Key=file.filename
-        ^^^^^^^^^^^^^^^^^
-    )
-    ^
-  File "C:\Users\hp\Documents\devs\back-endPanduLine\venv\Lib\site-packages\botocore\context.py", line 123, in wrapper
-    return func(*args, **kwargs)
-  File "C:\Users\hp\Documents\devs\back-endPanduLine\venv\Lib\site-packages\boto3\s3\inject.py", line 657, in upload_fileobj
-    raise ValueError('Fileobj must implement read')
-ValueError: Fileobj must implement read
-
-
+AKIA3QFM6OASSNO4CHWQ
+  yrmBgsrnXZGerrg3nBoma4oPkRArVkRU5Ueem+wD
 
 
 '''
