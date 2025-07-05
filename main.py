1b8d5e42e0d79930648bbcec06ec331d79d74e5f
Canderson-Gomes <candidogomescg@gmail.com>
2025-07-05 14:32:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 import boto3
+import json, io
 from botocore.exceptions import NoCredentialsError
 import os
 from dotenv import load_dotenv
@@ -18,9 +19,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 # Configurações da AWS
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-
+#AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+#AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_ACCESS_KEY_ID ="AKIA3QFM6OASSNO4CHWQ" #os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = "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"#os.getenv
 AWS_REGION = "eu-north-1"  # ou sua região
 BUCKET_NAME = "app-panduline"
 
@@ -35,18 +37,21 @@ async def getting():
     
     return {"api":"api no ar"}
     
-@app.post("/upload/")
+@app.post("/uploadst/")
 async def upload_file(file: UploadFile = File(...)):
     #
     #return {"sucesso":"Main sucess"}
     try:
         contents = await file.read()
+        file_stream=io.BytesIO(contents)
         s3.upload_fileobj(
-            Fileobj=bytes(contents),
+            Fileobj=file_stream,
             Bucket=BUCKET_NAME,
-            Key=file.filename
+            Key=file.filename,
+            ExtraArgs={"ContentType":file.content_type}
         )
         url = f"https://{BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{file.filename}"
+        print(url)
         return {"url": url}
     except NoCredentialsError:
         return {"error": "Credenciais inválidas"}
