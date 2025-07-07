769b8cf8035185bbe49fd7149428ffc0b92f3db0
Canderson-Gomes <candidogomescg@gmail.com>
2025-07-07 17:06:34 -0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, ARRAY, Float, TIMESTAMP
+from database import Base
+
+class Person(Base):
+    __tablename__ = "people"
+    id = Column(Integer, primary_key=True, index=True)
+    image_url = Column(String, nullable=False)
+    embedding = Column(ARRAY(Float), nullable=False)
+    created_at= Column(ARRAY, nullable=False)
