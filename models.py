e01478f2194f8f5e88cd5c7fcb488be26ec13f4b
Canderson-Gomes <candidogomescg@gmail.com>
2025-07-08 17:01:34 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, Integer, String, ARRAY, Float, TIMESTAMP
+from sqlalchemy import Column, Integer, String, Float, TIMESTAMP
+import sqlalchemy.dialects.postgresql import ARRAY
 from database import Base
 
 class Person(Base):
     __tablename__ = "people"
     id = Column(Integer, primary_key=True, index=True)
     image_url = Column(String, nullable=False)
-    embedding = Column(ARRAY(Float), nullable=False)
+    embedding = Column(ARRAY(Float))
     #created_at= Column(ARRAY, nullable=False)
