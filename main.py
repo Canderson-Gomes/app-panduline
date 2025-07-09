23ffa84f02ddae47127879227e4e93673e348407
Canderson-Gomes <candidogomescg@gmail.com>
2025-07-09 10:44:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from faiss_index import FaissIndex
 
   # ou qualquer valor que seja o dimensional do seu embedding
 #CONFIGURAR O DATA BASE------------------------------------------------#####################################################
-from sqlalchemy import create_engine, Column, Integer, String, ARRAY, TIMESTAMP
+from sqlalchemy import create_engine, Column, Integer, String, ARRAY
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
